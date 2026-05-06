5e5bec028c1f50863be6ff7ef997b79e55536f50
FrancescaL12 <lazotafrancesca@gmail.com>
2026-05-06 15:46:57 -0300

    TERMINE

--- main.js
+++ main.js
@@ -9,5 +9,46 @@ Distancia	  | Con SUBE registrada   | Con SUBE sin registrar
 */
 
 boton.addEventListener("click", () => {
-  // COMPLETAR
+  let registradaValor = registrada.value;
+  let distanciaValor = Number(distancia.value);
+  
+  let precio = 0;
+  
+  if (distanciaValor <= 3) {
+    if (registradaValor === "si") {
+      precio = 100;
+    } else {
+      precio = 120;
+    }
+  
+  } else if (distanciaValor <= 6) {
+    if (registradaValor === "si") {
+      precio = 120;
+    } else {
+      precio = 150;
+    }
+  
+  } else if (distanciaValor <= 12) {
+    if (registradaValor === "si") {
+      precio = 150;
+    } else {
+      precio = 180;
+    }
+  
+  } else if (distanciaValor <= 27) {
+    if (registradaValor === "si") {
+      precio = 180;
+    } else {
+      precio = 220;
+    }
+  
+  } else {
+    if (registradaValor === "si") {
+      precio = 220;
+    } else {
+      precio = 260;
+    }
+  }
+  
+  tarifa.innerText = "El valor del boleto es: $" + precio;
 });
