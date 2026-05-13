4949b7379bed6d942e1d35295593350438410650
FrancescaL12 <lazotafrancesca@gmail.com>
2026-05-13 16:02:17 -0300

    manijha

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ Distancia	  | Con SUBE registrada   | Con SUBE sin registrar
 6-12 km	    | $ 855.97		          | $ 1360.99
 12-27 km	  | $ 917.24		          | $ 1458.41
 */
-
 boton.addEventListener("click", () => {
   let registradaValor = registrada.value;
   let distanciaValor = Number(distancia.value);
+  const tipoColectivo = document.getElementById("tipoColectivo");
 
   let precio = 0;
 
@@ -18,6 +18,8 @@ boton.addEventListener("click", () => {
 
     if (registradaValor === "si") {
       precio = 715.24;
+    } else if (registradaValor === "tarifa social") {
+      precio = 339.18;
     } else {
       precio = 1137.23;
     }
@@ -26,6 +28,8 @@ boton.addEventListener("click", () => {
 
     if (registradaValor === "si") {
       precio = 794.74;
+    } else if (registradaValor === "tarifa social") {
+      precio = 376.88;
     } else {
       precio = 1263.64;
     }
@@ -34,6 +38,8 @@ boton.addEventListener("click", () => {
 
     if (registradaValor === "si") {
       precio = 855.97;
+    } else if (registradaValor === "tarifa social") {
+      precio = 405.91;
     } else {
       precio = 1360.99;
     }
@@ -42,9 +48,67 @@ boton.addEventListener("click", () => {
 
     if (registradaValor === "si") {
       precio = 917.24;
+    } else if (registradaValor === "tarifa social") {
+      precio = 434.97;
     } else {
       precio = 1458.41;
     }
+    else if (distanciaValor > 27) {
+      tarifa.innerText="Distancia Invalida"
+
+  }
+
+
+
+  if (tipoColectivo.value === "Provincia de Buenos Aires")
+  if (distanciaValor <= 3) {
+
+    if (registradaValor === "si") {
+      precio = 968.57;
+    } else if (registradaValor === "tarifa social") {
+      precio = 435.85;
+    } else {
+      precio = 1937.14;
+    }
+
+  } else if (distanciaValor <= 6) {
+
+    if (registradaValor === "si") {
+      precio = 1089.64;
+    } else if (registradaValor === "tarifa social") {
+      precio = 490.33;
+    } else {
+      precio = 2179.28;
+    }
+
+  } else if (distanciaValor <= 12) {
+
+    if (registradaValor === "si") {
+      precio = 1210.71;
+    } else if (registradaValor === "tarifa social") {
+      precio = 544.81;
+    } else {
+      precio = 2421.42;
+    }
+
+  } else if (distanciaValor <= 27) {
+
+    if (registradaValor === "si") {
+      precio = 1452.85;
+    } else if (registradaValor === "tarifa social") {
+      precio = 653.78;
+    } else {
+      precio = 2905.70;
+    }
+    else if (distanciaValor > 27) {
+
+      if (registradaValor === "si") {
+        precio = 1708.07;
+      } else if (registradaValor === "tarifa social") {
+        precio = 768.63;
+      } else {
+        precio = 3416.14;
+      }
 
   }
 
